2c837ef5587c9cb83410dd42a75797270ead5822
xc leung <497513735@qq.com>
2020-04-01 00:30:16 +0800

    2.02 finish

--- MVue.js
+++ MVue.js
@@ -2,11 +2,39 @@ class Compile {
   constructor(el, vm) {
     this.el = this.isElementNode(el) ? el : document.querySelector(el)
     this.vm = vm
-    //避免回流和重绘，获取文档碎片对象放到内存中
-    this.node2Fragment(this.el)
+    //1.避免回流和重绘，获取文档碎片对象放到内存中
+    const fragment = this.node2Fragment(this.el)
+    //2.编译模版
+    this.compile(fragment)
+    //3.追加子元素到根元素
+    this.el.appendChild(fragment)
   }
   isElementNode(node) {
     return node.nodeType === 1
+  }
+  node2Fragment(el) {
+    //创建文档碎片
+    const f = document.createDocumentFragment()
+    let firstChild
+    while (firstChild = el.firstChild) {
+      f.appendChild(firstChild)
+    }
+    return f
+  }
+  compile(fragment) {
+    const childNodes = fragment.childNodes
+    childNodes.forEach(child => {
+      if(this.isElementNode(child)) {
+        console.log('元素节点', child)
+        //TO DO 处理元素节点
+      } else {
+        console.log('文本节点', child)
+        //TO DO 处理文本节点
+      }
+      if(child.childNodes && child.childNodes.length) {
+        this.compile(child.childNodes)
+      }
+    })
   }
 }
 
